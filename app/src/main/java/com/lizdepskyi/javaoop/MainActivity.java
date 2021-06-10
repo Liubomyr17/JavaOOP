a96495c5985ccd4cb0da4216a6ab90c7e681b60a
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-10 19:07:57 +0300

    Test Commit

---
+++ app/src/main/java/com/lizdepskyi/javaoop/MainActivity.java
@@ -0,0 +1,28 @@
+package com.lizdepskyi.javaoop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    Cat myCat;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myCat = new Cat(4, "Puma");
+        myCat.talk();
+
+        Cat murzik = new Cat(2, "Murza");
+        murzik.talk();
+
+        Cat glasha = new Cat();
+        glasha.age = 1;
+        glasha.name = "Glasha";
+        glasha.talk();
+    }
+}
