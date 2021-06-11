1db9bc936b66eda08787d20a36d504fa8ebe6306
Liubomyr Izdepskyi <lizdepskij@gmail.com>
2021-06-11 18:11:48 +0300

    75. Inner class

--- app/src/main/java/com/lizdepskyi/javaoop/Cat.java
+++ app/src/main/java/com/lizdepskyi/javaoop/Cat.java
@@ -8,33 +8,70 @@ public class Cat extends Animal {
     String name;
     final static int numberOfLegs = 4;
     static int count = 0;
+    String helloText;
+    CatMood catMood;
+
+    class CatMood {
+        int levelOfMood;
+
+        CatMood() {
+            if (Cat.this.age < 2) {
+                levelOfMood = 100;
+            } else if (Cat.this.age < 7) {
+                levelOfMood = 50;
+            } else {
+                levelOfMood = 20;
+            }
+        }
+    }
 
     public Cat() {
         count++;
         this.name = "John Doe";
         this.age = -1;
+
+        catMood = new CatMood();
+
+        switch (catMood.levelOfMood) {
+            case 100:
+                helloText = "Meow! I'm happy Cat =) My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+            case 50:
+                helloText = "Meow! I'm cat =/ My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+            case 20:
+                helloText = "Meow! I'm old cat =( My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+        }
     }
 
     public Cat(int age, String name) {
         count++;
         this.age = age;
         this.name = name;
+
+        catMood = new CatMood();
+
+        switch (catMood.levelOfMood) {
+            case 100:
+                helloText = "Meow! I'm happy Cat =) My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+            case 50:
+                helloText = "Meow! I'm cat =/ My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+            case 20:
+                helloText = "Meow! I'm old cat =( My name is " + name +
+                        ", and I'm " + age + " years old.";
+                break;
+        }
     }
 
     public void talk() {
-        Log.i("talk()", "Meow! I'm Cat. My name is " + name +
-                ", and I'm " + age + " years old.");
-    }
-
-    public void talk(int age) {
-        Log.i("talk()", "Meow! I'm " + age + " years old.");
-    }
-
-    public void talk(String hello) {
-        Log.i("talk()", "Meow! " + hello);
-    }
-
-    public static String whatCatsLike() {
-        return " I like playing";
+        Log.i("talk()", helloText);
     }
 }
--- app/src/main/java/com/lizdepskyi/javaoop/MainActivity.java
+++ app/src/main/java/com/lizdepskyi/javaoop/MainActivity.java
@@ -16,9 +16,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Cat cat1 = new Cat();
-        Cat cat2 = new Cat();
+        cat1.talk();
+        Cat cat2 = new Cat(5, "Vaska");
+        cat2.talk();
+        Cat cat3 = new Cat(8, "Murzik");
+        cat3.talk();
 
-        Log.i("count", "count: " + String.valueOf(Cat.count));
+        Cat.CatMood catMood = new Cat().new CatMood();
+
+        Log.i("levelOfMood", String.valueOf(catMood.levelOfMood));
 
     }
 }
